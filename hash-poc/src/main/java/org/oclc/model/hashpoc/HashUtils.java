4344ea9c3bdd18c5fa3b5975eb7f3d52c3e74aa9
Hugh Jamieson <hjamieson5@gmail.com>
2017-01-06 08:11:13 -0500

    Added salt spec & tests

--- hash-poc/src/main/java/org/oclc/model/hashpoc/HashUtils.java
+++ hash-poc/src/main/java/org/oclc/model/hashpoc/HashUtils.java
@@ -12,6 +12,19 @@ public class HashUtils {
       HashFunction hf = Hashing.goodFastHash(16);
       HashCode hc = hf.hashBytes(bytes);
       return (short) (hc.asInt() % 512);
+   }
 
+   /**
+    * returns an 8-bit salt value that can be used for distribution on a rowkey.  NOte that
+    * it is only capable of expressing a 256 value, which would be the upper value for the
+    * number of regions.
+    * @param bytes  the bytes to use for the hash
+    * @return single byte with salt value
+    */
+   public static byte getSalt(byte[] bytes){
+      HashFunction hf = Hashing.goodFastHash(16);
+      HashCode hc = hf.hashBytes(bytes);
+      byte[] hash = hc.asBytes();
+      return (byte) hash[hash.length-1];
    }
 }
--- hash-poc/src/test/java/org/oclc/model/hashpoc/HashUtilsTest.java
+++ hash-poc/src/test/java/org/oclc/model/hashpoc/HashUtilsTest.java
@@ -6,6 +6,12 @@ import org.junit.Test;
 
 import javax.xml.bind.DatatypeConverter;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
@@ -13,6 +19,15 @@ import static org.junit.Assert.assertThat;
  * Created by hughj on 1/3/17.
  */
 public class HashUtilsTest {
+
+   public static final int TEST_SIZE = 256;
+   Stream<String> getIris(int count){
+      String[] strings = new String[count];
+      for (int i = 0; i < count; i++){
+         strings[i]= "http://oc.lc/ent/abc"+i;
+      }
+      return Arrays.stream(strings);
+   }
 
    @Test
    public void testGenHash() {
@@ -23,22 +38,32 @@ public class HashUtilsTest {
    }
 
    @Test
-   public void testShiftRight() {
-      int f = 0x0b0b0a0a;
-      int s = f >> 16;
-      assertThat(s, equalTo(0x0b0b));
-      assertThat(f & 0x0000ffff, equalTo(0x0a0a));
+   public void testGetSalt(){
+      int numSamples = 1000;
+      Stream<String> iris = getIris(numSamples);
+      Stream<Byte> salts = iris.map(s -> HashUtils.getSalt(s.getBytes()));
+      assertThat("we should get a diff salt for each",(int)salts.count(), equalTo(numSamples));
 
-      System.out.println(Integer.valueOf(-1).shortValue());
+      assertThat("expect ~2/1 distribution",getIris(numSamples).map(s -> HashUtils.getSalt(s.getBytes())).distinct().count(), lessThan((long)(numSamples/2)));
    }
 
    @Test
-   public void testSignedShift() {
-      int bits = Integer.MIN_VALUE;
-      System.out.println("bits = " + bits);
-      assertThat(bits, lessThan(0));
-      assertThat(bits >> 8, lessThan(0));
-      assertThat(bits >>> 8, greaterThan(0));
-      System.out.println(DatatypeConverter.printHexBinary(Ints.toByteArray(bits)));
+   public void testSaltDistribution(){
+      List<Byte> res = getIris(256).map(s -> HashUtils.getSalt(s.getBytes())).collect(Collectors.toList());
+      Stream<Byte> byteStream = res.stream();
+      assertThat("expect at least half of 256 distinct values",byteStream.distinct().count(), greaterThan(128l));
+   }
+
+   @Test
+   public void testSaltStability(){
+      byte[] store1 = new byte[TEST_SIZE];
+      byte[] store2 = new byte[TEST_SIZE];
+      for (int i=0; i < store1.length; i++){
+            store1[i] = HashUtils.getSalt(("http://oc.lc/ent/" + i).getBytes());
+            store2[i] = HashUtils.getSalt(("http://oc.lc/ent/" + i).getBytes());
+      }
+      for (int j = 0; j < store1.length; j++){
+         assertThat("salt value should be equal for same IRI",store1[j], equalTo(store2[j]));
+      }
    }
 }
