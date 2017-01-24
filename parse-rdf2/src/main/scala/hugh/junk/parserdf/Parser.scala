cb0de427aadebb18bb5c9f7e7c1e6709c1d0928d
Hugh Jamieson <jamiesoh@oclc.org>
2017-01-23 21:38:28 -0500

    more work on matching

--- parse-rdf2/src/main/scala/hugh/junk/parserdf/Parser.scala
+++ parse-rdf2/src/main/scala/hugh/junk/parserdf/Parser.scala
@@ -6,16 +6,32 @@ import scala.io.Source
   * Created by hughj on 1/12/17.
   */
 object Parser {
+
+  val spo = """^\s*(\S+)\s*(\S+)\s*(\S+)\s*\.$""".r
+  val spl = """^\s*(\S+)\s*(\S+)\s*(".+"(?=\s))\s\.$""".r
+  val sptl = """^\s*(\S+)\s*(\S+)\s*(".+"(?=\^)\^\^<[^>]+>)\s*\.$""".r
+  val spll = """^\s*(\S+)\s*(\S+)\s*("[^"]+"@\S+)\s*\.$""".r
+
   def main(args: Array[String]): Unit = {
 
     // make sure we are passed a file as the primary arg(0):
-    require(args.length > 0,"path to input file required!")
+    require(args.length > 0, "path to input file required!")
 
     // create the source
     val rawData = Source.fromFile(args(0))
     rawData.getLines() foreach println
 
     System.exit(0)
+  }
+
+  class Codon(sub: String, prd: String, objNode: ObjNode)
+
+  class ObjNode(nt: Int, value: String, lang: String, schema: String)
+
+  object Codon {
+    def apply(input: String): Codon = {
+      new Codon("", "", new ObjNode(0, "", "", ""))
+    }
   }
 
 }
--- parse-rdf2/src/test/scala/hugh/junk/parserdf/RegexTest.scala
+++ parse-rdf2/src/test/scala/hugh/junk/parserdf/RegexTest.scala
@@ -1,52 +1,76 @@
 package hugh.junk.parserdf
 
+import org.junit.runner.RunWith
 import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
 
 /**
   * Created by hughj on 1/13/17.
   */
+@RunWith(classOf[JUnitRunner])
 class RegexTest extends FunSuite {
-val items = io.Source.fromFile("src/test/resources/sample1.nt").getLines().toList
+  val items = io.Source.fromFile("src/test/resources/sample1.nt").getLines().toList
+  val spo = """^\s*(\S+)\s*(\S+)\s*(\S+)\s*\.$""".r
+  val spl = """^\s*(\S+)\s*(\S+)\s*(".+"(?=\s))\s\.$""".r
+  val sptl = """^\s*(\S+)\s*(\S+)\s*(".+"(?=\^)\^\^<[^>]+>)\s*\.$""".r
+  val spll = """^\s*(\S+)\s*(\S+)\s*("[^"]+"@\S+)\s*\.$""".r
 
-  test("I can see the test items from the test"){
+  test("I can see the test items from the test") {
     assert(items.size != 0)
   }
 
-  test("every item starts with a '<' or '_:'"){
-    assert(items map(s =>s.startsWith("<") || s.startsWith("_:")) reduceLeft(_ && _))
+  test("every item starts with a '<' or '_:'") {
+    assert(items map (s => s.startsWith("<") || s.startsWith("_:")) reduceLeft (_ && _))
   }
 
-  test("every item ends with a '.'"){
-    assert(items map(_.endsWith(".")) reduceLeft(_ && _))
+  test("every item ends with a '.'") {
+    assert(items map (_.endsWith(".")) reduceLeft (_ && _))
   }
 
-  test("we can isolate subject between < and >"){
-    val rx = """^<([^>]+).*""".r
+  test("all stmts composed of 3 globs + .") {
+
     val zzz = items.map(_ match {
-      case rx(sub) => sub
+      case spo(s, p, o) => (s, p, o)
+      case spl(s, p, o) => (s, p, o)
+      case sptl(s, p, o) => (s, p, o)
+      case spll(s, p, o) => (s, p, o)
     })
-    zzz.foreach(l => assert(!l.contains("<") && !l.contains(">")))
   }
 
-  test("we can isolate predicate between < and >"){
-    val rx = """^<([^>]+)>\s*<([^>]+)>.*""".r
+  test("subject is either <sub> or _: form") {
+    val zzz = items.map(_ match {
+      case spo(s, _, _) => s
+      case spl(s, _, _) => s
+      case sptl(s, _, _) => s
+      case spll(s, _, _) => s
+    })
+    zzz.foreach(l => assert(l.startsWith("<") || l.startsWith("_:")))
+  }
+
+  test("we can isolate predicate between < and >") {
     val preds = items.map(_ match {
-      case rx(sub, prd) => prd
+      case spo(_, p, _) => p
+      case spl(_, p, _) => p
+      case sptl(_, p, _) => p
+      case spll(_, p, _) => p
     })
-    preds.foreach(l => assert(!l.contains("<") && !l.contains(">")))
-//    preds foreach println
+    val px = """^<([^>]+)>$""".r
+    val preds2 = preds.map(_ match { case px(p) => p })
   }
 
-  test("we can isolate the object for further decoding"){
-    val rx = """^<([^>]+)>\s*<([^>]+)>\s*(.+)\.\s*$""".r
+  test("we can isolate the object for further decoding") {
     val objs = items.map(_ match {
-      case rx(_, _, obj) => obj.trim
+      case spl(_, _, o) => (o, "spl")
+      case sptl(_, _, o) => (o, "sptl")
+      case spll(_, _, o) => (o, "spll")
+      case spo(_, _, o) => (o, "spo")
     })
-    objs foreach println
-    objs.foreach(l => assert(!l.endsWith("."),"must not include period at end"))
-    objs.foreach(y => y(0) match {
-      case '"' => assert(y.endsWith("\""))
-      case '<' => assert(y.endsWith(">"))
+    objs.foreach(l => assert(!l._1.endsWith("."), "must not include period at end"))
+    objs.foreach(y => y._2 match {
+      case "spo" => assert(y._1.endsWith(">") || y._1.startsWith("_:"))
+      case "spl" => assert(y._1.endsWith("\""))
+      case "spll" => assert(y._1.matches(""".+@\S+"""))
+      case "sptl" => assert(y._1.startsWith("\"")&& y._1.endsWith(">"))
     })
   }
 
